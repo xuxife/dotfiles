d89fcc241135f1fa6db8f58496e37f360c850818
xuxife <xingfeixu@microsoft.com>
2021-09-26 18:58:06 +0800

    Add Fisher and modules: laughedelic/pisces, acomagu/fish-async-prompt

--- fish/config.fish
+++ fish/config.fish
@@ -12,3 +12,7 @@ if status is-interactive
     alias z='fasd_cd -d'     # cd, same functionality as j in autojump
     alias zz='fasd_cd -d -i' # cd with interactive selection
 end
+
+function replace_home_directory_in_fish_variables
+    sed -i '' "s|$HOME|~|g" ~/.config/fish/fish_variables
+end
