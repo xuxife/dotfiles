e1f32d825bd7fc856b76dd574c5b39ae80cfb2dc
xuxife <xuxife@gmail.com>
2022-10-28 01:05:57 +0800

    add functions

--- fish/config.fish
+++ fish/config.fish
@@ -6,50 +6,18 @@ if status is-interactive
     bind \cx\cc fzf-cd-widget
     bind \cg\cb fzf-git-branch-widget
     bind -M insert \cg\cb fzf-git-branch-widget
+
+    set -gx FZF_DEFAULT_COMMAND "fd --strip-cwd-prefix"
+    set -gx FZF_DEFAULT_OPTS "--height ~40% --multi --reverse --ansi \
+        --bind ctrl-n:preview-half-page-down,ctrl-p:preview-half-page-up \
+        --color=dark \
+        --color=fg:-1,bg:-1,hl:#5fff87,fg+:-1,bg+:-1,hl+:#ffaf5f \
+        --color=info:#af87ff,prompt:#5fff87,pointer:#ff87d7,marker:#ff87d7,spinner:#ff87d7"
 end
 
-function dev
-    set -l options (fish_opt -s s -l sync)
-    argparse $options -- $argv
-    if set -q _flag_sync
-        rsync -azvhP ~/repo/rp/ devbox:~/go/src/go.goms.io/aks/rp
-    end
-    ssh devbox
-end
-
-function fzf-git-branch-widget -d "Show git branch"
-    git branch | fzf --height=10% | string trim -c '* ' | read -l result
-    and commandline -i $result
-    commandline -f repaint
-end
-
-### START stash commandline
-function __commandline_stash -d 'Stash current command line'
-    set -g __stash_command_position (commandline -C)
-    set -g __stash_command (commandline -b)
-    commandline -r ""
-end
-
-function __commandline_pop -d 'Pop last stashed command line'
-    if not set -q __stash_command
-        return
-    end
-    commandline -r $__stash_command
-    if set -q __stash_command_position
-        commandline -C $__stash_command_position
-    end
-    set -e __stash_command
-    set -e __stash_command_position
-end
-
-function __commandline_toggle -d 'Stash current commandline if not empty, otherwise pop last stashed commandline'
-    set -l cmd (commandline -b)
-    if test "$cmd"
-        __commandline_stash
-    else
-        __commandline_pop
-    end
-end
-### END stash commandline
+set -gx PATH ~/.local/bin $PATH
+set -gx PATH ~/go/bin $PATH
+set -gx PATH ~/.krew/bin $PATH
+source ~/.local.config
 
 starship init fish | source
