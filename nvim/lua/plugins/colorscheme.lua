42ec4bd6331c544044dc550aed48e0e257fad1d7
Xingfei Xu <xingfeixu@microsoft.com>
2026-05-25 00:41:16 +0000

    vim: update background color

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -9,6 +9,18 @@ return {
         comments = { italic = true },
         keywords = { italic = false },
       },
+      color_overrides = {
+        light = {
+          background = "#f8efe7",
+        },
+      },
+      on_highlights = function(hl, c)
+        hl.Normal = { bg = "#f8efe7", fg = hl.Normal and hl.Normal.fg }
+        hl.NormalNC = { bg = "#f8efe7" }
+        hl.NormalFloat = { bg = "#f8efe7" }
+        hl.SignColumn = { bg = "#f8efe7" }
+        hl.EndOfBuffer = { bg = "#f8efe7" }
+      end,
     },
   },
 
